8b6bfd4202514cb8dad0253d352e9d088d468705
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-04-28 15:16:03 -0500

    read service operations

--- api/src/main/java/dml/domain/PlayerCharacterService.java
+++ api/src/main/java/dml/domain/PlayerCharacterService.java
@@ -5,6 +5,8 @@ import dml.models.PlayerCharacter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PlayerCharacterService {
 
@@ -22,6 +24,36 @@ public class PlayerCharacterService {
             result.addMessage("Character not found", ResultType.NOT_FOUND);
         }
         result.setPayload(character);
+        return result;
+    }
+
+    public Result<List<PlayerCharacter>> findByUser(Integer userId){
+        Result<List<PlayerCharacter>> result = new Result<>();
+
+        if (userId == null || userId <= 0){
+            result.addMessage("Needs valid id", ResultType.INVALID);
+            return result;
+        }
+        PlayerCharacter character = repo.findById(userId);
+        if (character == null){
+            result.addMessage("Character not found", ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    public Result<List<PlayerCharacter>> findByCampaign(Integer campaignId){
+        Result<List<PlayerCharacter>> result = new Result<>();
+
+        if (campaignId == null || campaignId <= 0){
+            result.addMessage("Needs valid id", ResultType.INVALID);
+            return result;
+        }
+        PlayerCharacter character = repo.findById(campaignId);
+        if (character == null){
+            result.addMessage("Character not found", ResultType.NOT_FOUND);
+        }
+
         return result;
     }
 
