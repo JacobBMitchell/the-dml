696e02c1dfd4293b656dd562438d3fc6c5f6f99a
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-04-28 13:54:52 -0500

    remember to 'npm install jwt-decode' front end started

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import AuthContext from "./AuthContext";
+import HomePage from "./HomePage";
+import Nav from "./Nav";
+import Login from "./Login";
+
 function App() {
+  const [user, setUser] = useState(null);
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <AuthContext.Provider value={[user, setUser]}>
+        <div className="App">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </div>
+      </AuthContext.Provider>
+    </BrowserRouter>
   );
 }
 
