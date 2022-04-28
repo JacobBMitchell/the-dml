d687bb15e6b745d53c700f0d97b3bbe7e85cb9c0
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-04-28 10:45:41 -0500

    first draft of java models

---
+++ api/src/main/java/dml/models/Character.java
@@ -0,0 +1,47 @@
+package dml.models;
+
+public class Character {
+    private Integer id;
+    private String name;
+    private Integer userId;
+    private Integer currentHealth;
+    private Integer maxHealth;
+    private DndClass dndClass;
+    private Race race;
+    private String alignment;
+    private Integer characterLevel;
+    private Integer armorClass;
+    private Integer speed;
+    private Integer str;
+    private Integer dex;
+    private Integer con;
+    private Integer intel;
+    private Integer wis;
+    private Integer cha;
+    boolean savingStr;
+    boolean savingDex;
+    boolean savingCon;
+    boolean savingIntel;
+    boolean savingWis;
+    boolean savingCha;
+    Boolean acrobatics;
+    Boolean animalHandling;
+    Boolean arcana;
+    Boolean athletics;
+    Boolean deception;
+    Boolean history;
+    Boolean insight;
+    Boolean intimidation;
+    Boolean investigation;
+    Boolean medicine;
+    Boolean nature;
+    Boolean perception;
+    Boolean performance;
+    Boolean persuasion;
+    Boolean religion;
+    Boolean sleightOfHand;
+    Boolean stealth;
+    Boolean survival;
+    //TODO: Store list of spell and weapon indexes;
+
+}
