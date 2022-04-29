f1f860be9c22cae8c9cca3bdcd67dbd4da90c8bd
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-04-29 14:15:16 -0500

    adding a controller class skeleton

---
+++ api/src/main/java/dml/controller/CharacterController.java
@@ -0,0 +1,50 @@
+package dml.controller;
+
+import dml.domain.PlayerCharacterService;
+import dml.models.PlayerCharacter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RestController
+@RequestMapping("/api/character")
+public class CharacterController {
+
+    @Autowired
+    PlayerCharacterService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity getCharacterById(@PathVariable Integer id, Principal User){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity getCharactersByUserId(@PathVariable Integer id, Principal User){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping("/campaign/{id}")
+    public ResponseEntity getCharactersByCampaignId(@PathVariable Integer id, Principal User){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @PostMapping
+    public ResponseEntity addCharacter(@RequestBody PlayerCharacter character, Principal User){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity updateCharacter(@RequestBody PlayerCharacter character, @PathVariable Integer id, Principal user){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteCharacter(@PathVariable Integer id, Principal user){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+}
