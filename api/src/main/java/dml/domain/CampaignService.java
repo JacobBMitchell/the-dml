1a9b2221d541befa99bf6bc7605cb82f079c98d5
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-05-03 11:32:48 -0500

    working on campaign service

--- api/src/main/java/dml/domain/CampaignService.java
+++ api/src/main/java/dml/domain/CampaignService.java
@@ -28,27 +28,57 @@ public class CampaignService {
         }
 
         Campaign campaign = repo.findById(id);
-        if (!campaign.getDmId().equals(requester.getUserId())){
+
+        if (!campaign.getDmId().equals(requester.getUserId()) || !requester.getRoles().contains("ADMIN")){
             result.addMessage("You cannot see this campaign", ResultType.INVALID);
+        }
+
+        if (result.isSuccess()){
+            result.setPayload(campaign);
         }
 
         return result;
     }
 
-    public Result<List<Campaign>> getCampaignsByUser(Integer uId, String Username){
+    public Result<List<Campaign>> getCampaignsByUser(Integer uId, String username){
         Result<List<Campaign>> result = new Result<>();
+
+        AppUser requester = userRepo.findByUsername(username);
+
+        if (requester == null || requester.getRoles().isEmpty()){
+            result.addMessage("Need to login", ResultType.INVALID);
+            return result;
+        }
+
+        if (!requester.getUserId().equals(uId) || !requester.getRoles().contains("ADMIN")){
+            result.addMessage("You do not have access to this data", ResultType.INVALID);
+        }
+
+        List<Campaign> campaigns = repo.findByUserId(uId);
+
+        if (result.isSuccess()){
+            result.setPayload(campaigns);
+        }
 
         return result;
     }
 
     public Result<Campaign> addCampaign(Campaign campaign,  String username){
-        Result<Campaign> result = new Result<>();
+        AppUser requester = userRepo.findByUsername(username);
+        Result<Campaign> result = validate(campaign, requester);
+
+        if (result.isSuccess()){
+            result.setPayload(repo.add(campaign));
+        }
 
         return result;
     }
 
+
     public Result<Campaign> update(Campaign campaign,  String username){
         Result<Campaign> result = new Result<>();
+
+
 
         return result;
     }
@@ -58,4 +88,22 @@ public class CampaignService {
 
         return result;
     }
+
+    private Result<Campaign> validate(Campaign campaign, AppUser requester) {
+        Result<Campaign> result = new Result<>();
+        if (campaign == null){
+            result.addMessage("Object Required", ResultType.INVALID);
+            return result;
+        }
+        if (campaign.getDmId() == null || !requester.getUserId().equals(campaign.getDmId()) || !requester.getRoles().contains("ADMIN")){
+            result.addMessage("You do not have access to this feature", ResultType.INVALID);
+        }
+        if (campaign.getPlayerIds() != null && campaign.getPlayerIds().stream().anyMatch(a -> userRepo.findById(a) == null)){
+            result.addMessage("Invalid player",ResultType.INVALID);
+        }
+
+
+        return result;
+    }
+
 }
