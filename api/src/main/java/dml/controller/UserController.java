228466b144713e93f1bf31b5d2bedf7c8fa517b8
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-05-04 11:02:52 -0500

    stuff

--- api/src/main/java/dml/controller/UserController.java
+++ api/src/main/java/dml/controller/UserController.java
@@ -1,11 +1,12 @@
 package dml.controller;
 
 import dml.domain.UserService;
+import dml.models.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
@@ -18,5 +19,11 @@ public class UserController {
     @GetMapping("/{username}")
     public ResponseEntity loadUserByUsername(@PathVariable String username){
         return ResponseEntity.ok(service.loadUserByUsername(username));
+    }
+
+    @PostMapping
+    public ResponseEntity createNewUser(@RequestBody AppUser user){
+
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
