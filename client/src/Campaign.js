7014b7b99b145ea4dd012d8d10cf4c56f1470384
tlitz10 <tlitz@dev-10.com>
2022-05-04 14:46:52 -0400

    added character and campaign info

--- client/src/Campaign.js
+++ client/src/Campaign.js
@@ -1,6 +1,22 @@
 function Campaign(props) {
+
+    const {campaign} = props;
+
     return (
-        <h5>{props.campaign.dmNotes}</h5>
+        <div className="campaign">
+        <div className="info">
+            <h4>DM: {campaign.dmId}</h4>
+            <h4>Notes:</h4>
+            <p>{campaign.dmNotes}</p>
+        </div>
+        <div className="Players">
+            <h4>Players:</h4>
+            <ul>
+                {campaign.playerIds.map(player => <li>{player}</li>)}
+            </ul>
+        </div>
+        
+    </div>
     )
 }
 
