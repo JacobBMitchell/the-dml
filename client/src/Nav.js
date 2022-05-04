c806e812096b5273b3dbc0d32b263e601183b7d6
tlitz10 <tlitz@dev-10.com>
2022-05-04 16:17:01 -0400

    fixed logout

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import AuthContext from './AuthContext';
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
     const [user, setUser] = useContext(AuthContext);
+    const navigate = useNavigate();
 
     function handleLogout() {
         localStorage.removeItem("token");
