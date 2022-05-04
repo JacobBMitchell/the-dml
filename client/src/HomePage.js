a2b69536913bfd653f364bdb5cdc8554de19c2a5
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-05-04 14:58:39 -0500

    bit of formatting

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,6 +1,12 @@
 function HomePage() {
     return (<div className="homepage">
         <h1>Welcome to the DUNGEON MASTER'S LAIR</h1>
+        <p>Greetings adventures! We are ROAD DOGS  and we have created a dungeons and dragons app that helps join players and
+        game masters in an easy and customizable environment. We allow our game masters access to character sheets from their 
+        campaigns to create better stories from the properties of the characters. As a game master myself I've had to hassle
+        around collecting and copying character sheets to get a feel for my player's characters, now we've molded it into one place.
+        As well as we've allowed for more customizable characters to allow for more homebrew and creativity at the table.
+        Our website is easy to use and contains tools and resources to make dungeons and dragons easy and exciting to play! </p>
         
     </div>)
 
