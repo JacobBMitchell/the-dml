f42d2771e7d941bf8ef37d6579a5e616858d8f71
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-05-04 15:14:28 -0500

    design work

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -7,7 +7,7 @@ function HomePage() {
         around collecting and copying character sheets to get a feel for my player's characters, now we've molded it into one place.
         As well as we've allowed for more customizable characters to allow for more homebrew and creativity at the table.
         Our website is easy to use and contains tools and resources to make dungeons and dragons easy and exciting to play! </p>
-        
+        <a href="/login"><h2>Get Started</h2></a>
     </div>)
 
 }
