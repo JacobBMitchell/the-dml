dc29cc177cc441c066da676f3fa5fcd8692e15f7
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-05-10 15:54:44 -0500

    update dm notes

--- client/src/Campaign.js
+++ client/src/Campaign.js
@@ -1,6 +1,52 @@
+import { useState } from "react";
+
 function Campaign(props) {
 
     const {campaign} = props;
+    const [isHidden, setIsHidden] = useState(true);
+
+    const editNotes = (ev) => {
+        setIsHidden(!isHidden);
+        const tbox = document.getElementById("article-input");
+        if (isHidden){
+            //edit
+
+        }
+        else {
+            let input = tbox.value;
+            campaign.dmNotes = input;
+            update();
+
+            //save and close
+        }
+    }
+
+    const update = () => {
+        fetch("http://localhost:8080/api/campaign/" + campaign.campaignId, {
+            method: "PUT",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("token"),
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(campaign)
+        })
+        .then(response => {
+            if(response.status == 201){
+                alert("saved")
+            } else {
+                alert("Campaign couldn't be updated.");
+                return response.json();
+            }
+        })
+        .then(jsonData => {
+            if(jsonData){
+                for(let i = 0; i < jsonData.length; i++){
+                    console.log(jsonData[i]);
+                }
+            }
+        })
+        .catch( rejection => console.log("Failed to update campaign. ", rejection));
+    }
 
     return (
         <div className="campaign">
@@ -9,6 +55,8 @@ function Campaign(props) {
             <h4>Name: {campaign.campaignName}</h4>
             <h4>Notes:</h4>
             <p>{campaign.dmNotes}</p>
+            <textarea id="article-input" type="text" className={isHidden ? "hidden" : ""}>{campaign.dmNotes}</textarea>
+            <button onClick={(ev) => editNotes(ev)}>{isHidden ? "Edit Notes" : "Save and Close"}</button>
         </div>
         <div className="Players">
             <h4>Players:</h4>
